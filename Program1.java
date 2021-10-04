9edc09336b53177dd8c99e75f403f67c96ddfa15
Ballaxmi <86150254+Ballaxmi@users.noreply.github.com>
2021-10-04 12:53:50 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,17 @@
+
+public class Program1 {
+	int number;
+	float rational;
+	char ch;
+	boolean bool;
+	String str;
+	public static void main(String[] args) {
+		Program1 pgm=new Program1();
+		System.out.println(pgm.number);
+		System.out.println(pgm.rational);
+		System.out.println(pgm.ch);
+		System.out.println(pgm.bool);
+		System.out.println(pgm.str);
+	}
+
+}
